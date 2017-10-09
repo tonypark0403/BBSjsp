b9fd8659ad2becfd948ff9da7e2471a99bf688a8
Tony Park <c3dream@gmail.com>
2017-10-08 22:43:13 -0400

    added login page and mysql setup

---
+++ BBS/src/user/User.java
@@ -0,0 +1,40 @@
+package user;
+
+public class User {
+	private String userID;
+	private String userPasword;
+	private String userName;
+	private String userGender;
+	private String userEmail;
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPasword() {
+		return userPasword;
+	}
+	public void setUserPasword(String userPasword) {
+		this.userPasword = userPasword;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+}
